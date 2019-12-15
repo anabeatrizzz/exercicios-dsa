8f6de78648dcc71bb4c862cf143325cf5ff6b382
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2019-12-15 15:39:16 -0300

    Add files via upload

---
+++ Capitulo 02 - Exercicios/Cap02-Exercicios.py
@@ -0,0 +1,66 @@
+# Os enunciados: https://github.com/dsacademybr/PythonFundamentos/blob/master/Cap02/Notebooks/DSA-Python-Cap02-Exercicios.ipynb
+
+print("Exercicio 1")
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for c in numeros:
+  print(f'{c} ', end="")
+
+print("\n")
+print("Exercicio 2")
+lista = [[1], [2], [3], [4], [5]]
+for c in lista:
+  print(f'{c} ', end="")
+
+print("\n")
+print("Exercicio 3")
+string1 = "Ana "
+string2 = "Beatriz"
+string3 = string1 + string2
+print(string3)
+
+print("\n")
+print("Exercicio 4")
+tupla = (1, 2, 2, 3, 4, 4, 4, 5)
+print(tupla.count(4))
+
+print("\n")
+print("Exercicio 5")
+dicionario = {}
+print(dicionario)
+
+print("\n")
+print("Exercicio 6")
+dicionario2 = {
+  "nome": "Ana",
+  "idade": 18,
+  "país": "Brasil"
+}
+for c, v in dicionario2.items():
+  print(c, v)
+
+print("\n")
+print("Exercicio 7")
+dicionario2['cor_preferida'] = 'vermelho'
+for c, v in dicionario2.items():
+  print(c, v)
+
+print("\n")
+print("Exercicio 8")
+dicionario3 = {
+  "endereço": "Rua Dos Bobos",
+  "numero": 0,
+  "pessoas": [2, 6]
+}
+for c, v in dicionario3.items():
+  print(c, v)
+
+print("\n")
+print("Exercicio 9")
+lista2 = ["Ana", [1, 2], {"objeto": "celular", "cor": "preto"}, 10.00]
+for c in lista2:
+  print(c)
+
+print("\n")
+print("Exercicio 10")
+frase = 'Cientista de Dados é o profissional mais sexy do século XXI'
+print(frase[1:18])
