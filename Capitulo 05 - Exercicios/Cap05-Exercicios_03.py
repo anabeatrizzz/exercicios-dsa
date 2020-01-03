71c09f9c3a65c4ecac9fa539de0d1635a401ac60
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2020-01-03 09:59:06 -0300

    Update Cap05-Exercicios_03.py

--- Capitulo 05 - Exercicios/Cap05-Exercicios_03.py
+++ Capitulo 05 - Exercicios/Cap05-Exercicios_03.py
@@ -1,4 +1,7 @@
-# Exercício 3 - Crie a classe Smartphone com 2 atributos, tamanho e interface e crie a classe MP3Player com os atributos capacidade. A classe MP3player deve herdar os atributos da classe Smartphone.
+"""Exercício 3 - Crie a classe Smartphone com 2 atributos, tamanho e interface
+e crie a classe MP3Player com os atributos capacidade.
+A classe MP3player deve herdar os atributos da classe Smartphone."""
+
 class Smartphone():
   def __init__(self, t, i):
     self.tamanho = t
