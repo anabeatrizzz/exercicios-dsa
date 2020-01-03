8c37b3ff0bd977b1c5b4794e21fde728d32913db
Ana Beatriz <53445202+anabeatrizzz@users.noreply.github.com>
2020-01-03 09:56:20 -0300

    Update Cap04-ExercicioS_05.py

--- Capitulo 04 - Exercicios/Cap04-ExercicioS_05.py
+++ Capitulo 04 - Exercicios/Cap04-ExercicioS_05.py
@@ -1,4 +1,5 @@
-# Exercício 5 - Abaixo você encontra duas listas. Faça com que cada elemento da listaA seja elevado ao elemento correspondente na listaB.
+"""Exercício 5 - Abaixo você encontra duas listas.
+Faça com que cada elemento da listaA seja elevado ao elemento correspondente na listaB."""
 
 listaA = [2, 3, 4]
 listaB = [10, 11, 12]
